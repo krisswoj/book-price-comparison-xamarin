3d344a68d513c2b45ec043c3b904f853788d8c69
Krzysztof Wojdak <krisswoj@gmail.com>
2019-01-21 20:23:51 +0100

    added first version of application

---
+++ BooksPriceCompare/BooksPriceCompare.Android/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Widget;
+using Android.OS;
+using System.Linq;
+
+namespace BooksPriceCompare.Droid
+{
+    [Activity(Label = "BooksPriceCompare",
+        Icon = "@mipmap/icon",
+        Theme = "@style/MainTheme",
+        MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        BooksViewModel _viewModel;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+//            ToolbarResource = Resource.Layout.Toolbar;
+            SetContentView(Resource.Layout.Toolbar);
+            
+            _viewModel = new BooksViewModel();
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            var list = FindViewById<ListView>(Resource.Id.listView1);
+
+            button.Click += async delegate
+            {
+                button.Enabled = false;
+
+                await _viewModel.GetBooksAsAsync();
+
+                list.Adapter = new ArrayAdapter<string>(this,
+                    Android.Resource.Layout.SimpleListItem1,
+                    Android.Resource.Id.Text1,
+                    _viewModel.Books.Select(m => $"{m.Name} - from {m.Location}").ToArray());
+
+                button.Enabled = true;
+            };
+        }
+    }
+}
---
+++ BooksPriceCompare/BooksPriceCompare/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BooksPriceCompare
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Details { get; set; }
+        public string Image { get; set; }
+        public int Population { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class BooksViewModel
+    {
+        public ObservableCollection<Book> Books { get; } = new ObservableCollection<Book>();
+
+        public async Task GetBooksAsAsync()
+        {
+            try
+            {
+                var client = new HttpClient();
+                var json = await client.GetStringAsync("https://raw.githubusercontent.com/jamesmontemagno/app-monkeys/master/MonkeysApp/monkeydata.json");
+
+                var items = JsonConvert.DeserializeObject<List<Book>>(json);
+
+                foreach (var item in items)
+                    Books.Add(item);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
